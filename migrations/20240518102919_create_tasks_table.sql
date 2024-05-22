8e02c09a93dc8d3e622c053de5bd8efd1ad0a330
MaksimovDenis <maksimovis74@gmail.com>
2024-05-22 23:49:00 +0300

    migrations have been updated

--- migrations/20240518102919_create_tasks_table.sql
+++ migrations/20240518102919_create_tasks_table.sql
@@ -26,29 +26,25 @@ CREATE TABLE tasks_labels(
     label_id INTEGER REFERENCES labels(id)
 );
 
--- Добавление данных в таблицу users
 INSERT INTO users (name) VALUES
 ('Alice'),
 ('Bob'),
 ('Charlie');
 
--- Добавление данных в таблицу labels
 INSERT INTO labels (name) VALUES
 ('Urgent'),
 ('Bug'),
 ('Feature');
 
--- Добавление данных в таблицу tasks
 INSERT INTO tasks (opened, closed, author_id, assigned_id, title, content) VALUES
 (CURRENT_TIMESTAMP, NULL, 1, 2, 'Fix login issue', 'User unable to login with correct credentials'),
 (CURRENT_TIMESTAMP, NULL, 2, 3, 'Add user profile page', 'Create a new page for user profiles'),
 (CURRENT_TIMESTAMP, NULL, 3, 1, 'Update password policy', 'Password must be at least 12 characters long');
 
--- Добавление связей между tasks и labels
 INSERT INTO tasks_labels (task_id, label_id) VALUES
-(1, 2), -- 'Fix login issue' has 'Bug' label
-(2, 3), -- 'Add user profile page' has 'Feature' label
-(3, 1); -- 'Update password policy' has 'Urgent' label
+(1, 2), 
+(2, 3), 
+(3, 1); 
 
 -- +goose Down
 DROP TABLE tasks_labels;
